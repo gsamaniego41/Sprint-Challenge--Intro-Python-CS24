680834fa1f274b4f6354b84a1f10f8ce364162fa
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-12-06 10:52:39 -0800

    uncommented cities.append

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -40,7 +40,7 @@ def cityreader(cities=[]):
 
         for row in csv_reader:
             print(row)
-            # cities.append(City(row[0], float(row[3]), float(row[4])))
+            cities.append(City(row[0], float(row[3]), float(row[4])))
 
         # print(csv_reader)
         return cities
